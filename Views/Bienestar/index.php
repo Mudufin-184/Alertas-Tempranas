0c269b2971dbdbaf97ed43ce786678e92d7707c7
Mudufin-184 <velandiafonseca@gmail.com>
2024-08-21 21:40:41 -0500

    Arreglo de title para que diga <title>AlertasTempranas</title>

--- Views/Bienestar/index.php
+++ Views/Bienestar/index.php
@@ -12,7 +12,7 @@ require_once("../../Controllers/bienestar/mostrar_info.php");
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <title>AlertasTempranas</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
--- Views/coordinador/registrarAprendiz.php
+++ Views/coordinador/registrarAprendiz.php
@@ -18,7 +18,7 @@ $encargados = $objConsultas->CargarEncargado();
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <title>AlertasTempranas</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
